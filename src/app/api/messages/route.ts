1efde8bf5086c3e6d209ecea57fcef2d6b186bcf
Tielman Nieuwoudt <tielman@aerobotics.com>
2024-01-26 22:06:04 +0200

    feat: add api endpoints for queue worker

---
+++ src/app/api/messages/route.ts
@@ -0,0 +1,57 @@
+import { db } from "@/database/db";
+import { messageTable } from "@/database/schema";
+import dayjs from "dayjs";
+import { asc, eq } from "drizzle-orm";
+
+export const dynamic = "force-dynamic";
+
+// TODO: Auth. Internal services want to communicate with this
+// Some "token" implementation can work - i.e. just share a long string
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const topicIdQueryParam = searchParams.get("topicId");
+  const topicId = parseInt(topicIdQueryParam!);
+
+  if (!topicIdQueryParam && Number.isNaN(topicId)) {
+    return new Response(null, {
+      status: 400,
+      statusText: "topicId required",
+    });
+  }
+
+  const messages = await db.query.messageTable.findMany({
+    where: eq(messageTable.topicId, topicId),
+    orderBy: [asc(messageTable.createdAt)],
+  });
+
+  return Response.json(messages);
+}
+
+export async function POST(request: Request) {
+  // This endpoint only serves our own queue worker, so a long-lived token should do.
+
+  let params;
+  try {
+    params = await request.json();
+    // Missing step: Validate request structure. Zod?
+  } catch (e) {
+    return new Response(null, {
+      status: 400,
+      statusText: "Request body required in JSON format",
+    });
+  }
+
+  const topicId = params.topicId;
+  const content = params.content;
+  const userId = params.userId;
+
+  await db.insert(messageTable).values({
+    createdAt: dayjs().toISOString(), // This should sit on a "serializer". Should be built in
+    topicId,
+    userId,
+    text: content,
+  });
+
+  return Response.json({ status: "ok" });
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,15 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: (req) => {
+    if (req.url === "/") return true;
+    // TODO: Make this check more secure
+    if (req.url.includes("/api/")) return true;
+    return false;
+  },
 });
 
 // Note: This is a pretty blind copy from Clerk docs. Definitely can be optimised.
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
 };
