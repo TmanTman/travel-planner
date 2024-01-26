5b2ec79c6448d605e4474616f5c4f72eebe5c555
Tielman Nieuwoudt <tielman@aerobotics.com>
2024-01-26 22:59:59 +0200

    fix: add signin pages to public pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextRequest } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: (req) => {
-    if (req.url === "/") return true;
+  publicRoutes: (req: NextRequest) => {
+    if (req.nextUrl.pathname === "/") return true;
+    if (["/sign-in", "/sign-up"].includes(req.nextUrl.pathname)) return true;
     // TODO: Make this check more secure
     if (req.url.includes("/api/")) return true;
     return false;
@@ -14,5 +16,5 @@ export default authMiddleware({
 
 // Note: This is a pretty blind copy from Clerk docs. Definitely can be optimised.
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/sign-in", "/sign-up"],
 };
